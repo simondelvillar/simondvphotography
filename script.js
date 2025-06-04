e14b49ec725a8bddb7c7754a4fd827c7ee142b2b
simondelvillar <simon.joven.delvillar@gmail.com>
2025-06-03 18:22:33 -0700

    Hamburger menu

--- script.js
+++ script.js
@@ -168,3 +168,11 @@ document.addEventListener('keydown', (e) => {
     lightboxClose.click();
   }
 });
+
+
+const hamburger = document.getElementById('hamburger');
+  const navLinks = document.getElementById('nav-links');
+
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+  });
